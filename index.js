142963f8ff1d2ce6da410884ab461117c386c24c
xian0831 <xian0831@gmail.com>
2017-02-09 22:43:56 -0500

    keep null values

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = filterObjectOnSchema;
 
 
-function filterObjectOnSchema(schema, doc){
+function filterObjectOnSchema(schema, doc) {
 
     var results;
 
@@ -9,47 +9,50 @@ function filterObjectOnSchema(schema, doc){
     //console.log("SCH: ", JSON.stringify(schema, null, 2));
 
     if (schema.type == 'object') {
-      results = {};   // holds this levels items
+        results = {}; // holds this levels items
 
-      // process properties  -  recursive
-      Object.keys(schema.properties).forEach(function(key){
-        if (doc[key] !== undefined){
-          var sp = schema.properties[key];
-          if ( sp.type == 'object'){
+        // process properties  -  recursive
+        Object.keys(schema.properties).forEach(function(key) {
+            if (doc[key] !== undefined) {
+                if (doc[key] === null) {
+                    results[key] = doc[key];
+                } else {
+                    var sp = schema.properties[key];
+                    if (sp.type == 'object') {
 
-            // check if property-less object (free-form)
-            if (sp.hasOwnProperty('properties')){
-              results[key] = filterObjectOnSchema(sp, doc[key]);
-            } else {
-              if (Object.keys(doc[key]).length > 0){
-                results[key] = doc[key];
-              } 
+                        // check if property-less object (free-form)
+                        if (sp.hasOwnProperty('properties')) {
+                            results[key] = filterObjectOnSchema(sp, doc[key]);
+                        } else {
+                            if (Object.keys(doc[key]).length > 0) {
+                                results[key] = doc[key];
+                            }
+                        }
+
+                    } else if (sp.type == 'array') {
+                        if (doc[key]) results[key] = filterObjectOnSchema(sp, doc[key]);
+                    } else if (sp.type == 'boolean' || sp.type == 'number' || sp.type == 'integer' || sp.type == 'string') {
+                        if (typeof doc[key] != 'undefined') results[key] = doc[key];
+                    } else {
+                        if (doc[key]) results[key] = doc[key];
+                    }
+                }
             }
 
-          }else if(sp.type == 'array'){
-            if (doc[key]) results[key] = filterObjectOnSchema(sp, doc[key]);
-          }
-          else if(sp.type == 'boolean' || sp.type == 'number' || sp.type == 'integer'){
-            if(doc[key] != null && typeof doc[key] != 'undefined') results[key] = doc[key];
-          }else{
-            if (doc[key]) results[key] = doc[key]; 
-          }
-        }
-      });
-
-    }else if (schema.type == 'array'){
-      // arrays can hold objects or literals
-      if (schema.items.type == 'object') {
-        results = [];
-        doc.forEach(function (item) {
-          results.push(filterObjectOnSchema(schema.items, item));
         });
-      } else {
-        results = doc;
-      }
+
+    } else if (schema.type == 'array') {
+        // arrays can hold objects or literals
+        if (schema.items.type == 'object') {
+            results = [];
+            doc.forEach(function(item) {
+                results.push(filterObjectOnSchema(schema.items, item));
+            });
+        } else {
+            results = doc;
+        }
     }
 
     return results;
 
 }
-
